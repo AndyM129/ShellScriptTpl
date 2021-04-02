0a4f3586f7c6de5dbe7076bf114a99a582e6da21
mengxinxin <mengxinxin@baidu.com>
2021-04-02 18:40:58 +0800

    提交1.1.0版本，并补充文档

--- README.md
+++ README.md
@@ -1,2 +1,196 @@
-# ShellScriptTpl
-Shell 脚本模板，已预制常用常量、变量、方法定义，及复杂参数解析，以便快速开始核心编程。
+# script_tpl.sh
+
+## Introduction
+
+`script_tpl.sh` 是一个Shell 脚本模板
+
+通过预制常用常量、变量、方法定义，及复杂参数解析
+
+实现快速开发的目的
+
+## Features
+
+1. 预定义 文件、版权、版本、更新日志 等的版权信息
+2. 预定义 系统版本、时间、路径等运行时信息
+3. 预定义 `debug`、`verbose` 模式，以控制输出
+4. 预定义 `echoDebug`、`echoInfo`、`echoSuccess`、`echoWarn`、`echoError`、`echoFatal` 等分级信息输出
+5. 预定义 "参数解析"、”使用说明“ 等核心处理
+
+## Usage
+
+可通过 `sh script_tpl.sh -h` 来查看使用说明：
+```shell
+Andys-MacBook-Pro-2018:ShellScriptTpl mengxinxin$ sh script_tpl.sh -h
+脚本名称: script_tpl.sh
+功能简介: Shell 脚本模板，已预制常用常量、变量、方法定义，及复杂参数解析，以便快速开始核心编程。
+当前版本: 1.1.0
+最近更新: 2021/04/01
+作    者: MengXinxin <andy_m129@163.com>
+说明文档: https://github.com/AndyM129
+
+=============================================================
+
+Usage:
+
+	$ sh script_tpl.sh <command> [params...] [--Option [value] [-sub_option [value]]...]...
+
+Commands:
+	command1:	命令1
+	command2:	命令2
+
+Options:
+	--opt1:		选项1
+	--opt2:		选项2
+	--updatelog:	脚本的更新日志
+	--version:	当前脚本版本
+	--help:		查看使用说明
+
+SubOptions:
+	-sub_opt1:	子选项1
+	-sub_opt2:	子选项2
+Andys-MacBook-Pro-2018:ShellScriptTpl mengxinxin$ 
+```
+
+![image-20210402174222623](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183643.png)
+
+### Example 1：类似 `find` 的系统风格
+
+
+```sh
+# 套用如下示例
+find ./ -maxdepth 1  -type d  ! -name "hhh"
+find ./test -path "test/test4" -prune -o -print
+
+# 当前脚本的执行方式&效果如下
+sh script_tpl.sh ./ -maxdepth 1  -type d  ! -name "hhh"
+sh script_tpl.sh ./test -path "test/test4" -prune -o -print
+```
+
+|   命令1   |   命令2   |
+| ---- | ---- |
+|   ![image-20210402175255800](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183940.png)   |  ![image-20210402175618227](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183948.png)   |
+
+### Example 2：类似`xcodebuild` 的分离可选参数风格
+
+```shell
+# 套用如下示例
+xcodebuild -workspace Example.xcworkspace -scheme Example -sdk iphoneos -configuration Release clean archive -archivePath "~/Desktop/Archive"
+
+# 当前脚本的执行方式&效果如下
+sh script_tpl.sh -workspace Example.xcworkspace -scheme Example -sdk iphoneos -configuration Release clean archive -archivePath "~/Desktop/Archive"
+```
+
+![image-20210402181246083](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183713.png)
+
+### Example 3：类似 `git` 的命令风格
+
+```shell
+# 套用如下示例
+git commt -m "commit something"
+git push origin master
+
+# 当前脚本的执行方式&效果如下
+sh script_tpl.sh commt -m "commit something"
+sh script_tpl.sh push origin master
+```
+
+| 命令1                                                        | 命令2                                                        |
+| ------------------------------------------------------------ | ------------------------------------------------------------ |
+| ![image-20210402180429547](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183853.png)|![image-20210402180553912](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183902.png) |
+
+### Example 4：全能的混合风格
+
+```shell
+sh script_tpl.sh -v command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"# sh template_2.sh -v command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"
+```
+
+![image-20210402181514897](https://raw.githubusercontent.com/AndyM129/ImageHosting/master/images/20210402183724.png)
+
+### Example ∞：其他
+
+```shell
+sh script_tpl.sh command p1 p2 p3 --option1 -subOption1 aaa -subOption2 bbb --option2 -subOption1 ccc -subOption2 ddd
+
+sh script_tpl.sh command p1 p2 p3 "p4,p5,p6" --option1 -subOption1 aaa -subOption2 bbb --option2 -subOption1 ccc -subOption2 ddd
+
+sh script_tpl.sh command p1 p2 p3 p4,p5,p6 --option1 -subOption1 aaa -subOption2 bbb --option2 -subOption1 ccc -subOption2 ddd
+
+sh script_tpl.sh command p1 p2 p3 p4,p5,p6 --option1 -subOption1 aaa -subOption2 bbb --option2 22 -subOption1 ccc -subOption2 ddd eee
+
+sh script_tpl.sh command p1 p2 p3 p4,p5,p6 --option1 -subOption1 "a a a" -subOption2 bbb --option2 22 -subOption1 ccc -subOption2 ddd eee
+
+sh script_tpl.sh -dv command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"
+
+sh script_tpl.sh -v command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"
+
+sh script_tpl.sh -d command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"
+
+```
+
+
+
+## Install （⭐️ Recommended）
+
+> 注：以下内容为针对 MacOS 进行的配置&说明。
+
+以上，是脚本的基础用法，但是更推荐直接安装到本地，即可 像执行系统命令一样的进行备份，具体方法如下。
+
+1. 打开`~/.bashrc`文件，并追加如下代码
+
+  ```shell
+  ##############################【Backup】#################################
+  alias sst.install='install_path="/Users/$USER/.bash_files/ShellScriptTpl"; git_url="https://github.com/AndyM129/ShellScriptTpl.git"; rm -rf "$install_path"; git clone $git_url $install_path; echo "script_tpl.sh install success: $install_path"; open $install_path;'
+  alias sst.opendir='open /Users/$USER/Documents/ShellScriptTpl'
+  ```
+  
+2. 执行如下命令，以便让修改生效
+
+	```shell
+	source ~/.bashrc # 可在任意目录下执行
+	```
+
+3. 安装
+
+	```shell
+	sst.install # 可在任意目录下执行
+	```
+
+4. 完成
+
+	
+
+## UpdateLog
+
+### 2021/03/31: v1.0.0 
+* 立项，参考 `xcodebuild` 命令，实现基于参数键值对方式的参数处理，如：
+
+	```shell
+	sh template.sh --command1 -option11 aaa -option12 bbb --command2 -option21 ccc -option22 ddd eee fff ggg
+	```
+
+### 2021/04/01: v1.1.0
+* 参考 `pod` 命令，扩展实现 支持命令、选项、参数，或同时输入的参数处理，如：
+
+	```shell
+	sh template_2.sh -d command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"
+	```
+
+	
+
+## Author
+
+AndyMeng, andy_m129@163.com
+
+If you have any question with using it, you can email to me. 
+
+## Collaboration
+
+Feel free to collaborate with ideas, issues and/or pull requests.
+
+## License
+
+AMKCategories is available under the MIT license. See the LICENSE file for more info.
+
+
+
+
--- script_tpl.sh
+++ script_tpl.sh
@@ -2,161 +2,186 @@
 
 # =========================================== COPYRIGHT ===========================================
 readonly SCRIPT_NAME="script_tpl.sh" # 脚本名称
-readonly SCRIPT_DESC="Shell 脚本模板" # 脚本名称
-readonly CURRENT_VERSION="1.0.0" # 脚本版本
-readonly LAST_UPDATETIME="2021/03/31" # 最近的更新时间
+readonly SCRIPT_DESC="Shell 脚本模板，已预制常用常量、变量、方法定义，及复杂参数解析，以便快速开始核心编程。" # 脚本名称
+readonly SCRIPT_VERSION="1.1.0" # 脚本版本
+readonly SCRIPT_UPDATETIME="2021/04/01" # 最近的更新时间
 readonly AUTHER_NAME="MengXinxin" # 作者
 readonly AUTHER_EMAIL="andy_m129@163.com" # 作者邮箱
 readonly REAMDME_URL="https://github.com/AndyM129/ShellScriptTpl" # 说明文档
+readonly SCRIPT_UPDATE_LOG=''' 
+\n### 2021/03/31: v1.0.0 
+\n
+\n* 立项，参考 `xcodebuild` 命令，实现基于参数键值对方式的参数处理，如：
+\n
+\n  ```shell
+\n  sh template.sh --command1 -option11 aaa -option12 bbb --command2 -option21 ccc -option22 ddd eee fff ggg
+\n  ```
+\n
+\n### 2021/04/01: v1.1.0
+\n
+\n* 参考 `pod` 命令，扩展实现 支持命令、选项、参数，或同时输入的参数处理，如：
+\n
+\n  ```shell
+\n  sh template_2.sh -d command p1 p2 p3 p4,p5,p6 "p7 p8 p9" --option1 -subOption1 -subOption2 --option2 aaa -subOption1 bbb --option3 "ccc" -subOption1 "ddd" -subOption2 eee "fff ggg"
+\n  ```
+\n
+'''
 
 # =========================================== GLOBAL CONST ===========================================
-readonly MACOS_VER="$(/usr/bin/sw_vers -productVersion)" # 当前 MacOS 版本
-readonly DATE=$(date "+%Y-%m-%d %H:%M:%S") # 当前时间
-readonly DATE_STAMP=${DATE//-: /} # 当前时间戳
-readonly DIRPATH=$(dirname $0); # 文件路径
-readonly BASENAME=$(basename $0); # 文件名
-readonly BASENAME_WITHOUT_SUFFIX=${BASENAME%.*} # 文件名（不含后缀）
-readonly BASENAME_SUFFIX=${BASENAME##*.} # 文件后缀
+readonly MACOS_VER="$(/usr/bin/sw_vers -productVersion)" # 当前 MacOS 版本，eg. 11.0.1
+readonly TIMESTAMP=$(date +%s) # 当前时间戳，eg. 1617351251
+readonly DATE=$(date -r $TIMESTAMP "+%Y-%m-%d %H:%M:%S") # 当前时间，eg. 2021-04-02 16:14:11
+readonly DATE_STAMP=$(date -r $TIMESTAMP "+%Y%m%d%H%M%S") # 当前时间戳，eg. 20210402161411
+readonly CURRENT_PATH=$(pwd); # 当前所在路径
+readonly SCRIPT_DIRPATH=$(dirname $0); # 当前脚本的文件路径
+readonly SCRIPT_BASENAME=$(basename $0); # 当前脚本的文件名
+readonly SCRIPT_BASENAME_WITHOUT_SUFFIX=${SCRIPT_BASENAME%.*} # 文件名（不含后缀）
+readonly SCRIPT_BASENAME_SUFFIX=${SCRIPT_BASENAME##*.} # 文件后缀
 
 # =========================================== GLOBAL VARIABLES ===========================================
-verbose="1"
+verbose="0"
 debug="0"
 
 # =========================================== GLOBAL FUNCTIONS ===========================================
-echo_debug() { if [[ $verbose == "1" || $debug == "1" ]]; then echo "\033[1;2m$@\033[0m"; fi;}      # debug 级别最低，可以随意的使用于任何觉得有利于在调试时更详细的了解系统运行状态的东东；
-echo_info() { echo "\033[1;36m$@\033[0m"; }      # info  重要，输出信息：用来反馈系统的当前状态给最终用户的；
-echo_success() { echo "\033[1;32m$@\033[0m"; }   # success 成功，输出信息：用来反馈系统的当前状态给最终用户的；
-echo_warn() { echo "\033[1;33m$@\033[0m"; }      # warn, 可修复，系统可继续运行下去；
-echo_error() { echo "\033[1;31m$@\033[0m"; }     # error, 可修复性，但无法确定系统会正常的工作下去;
-echo_fatal() { echo "\033[5;31m$@\033[0m"; }     # fatal, 相当严重，可以肯定这种错误已经无法修复，并且如果系统继续运行下去的话后果严重。
-git_branch_name() { git symbolic-ref --short HEAD; }
+echoDebug() { if [[ $verbose == "1" || $debug == "1" ]]; then echo "\033[1;2m$@\033[0m"; fi;}      # debug 级别最低，可以随意的使用于任何觉得有利于在调试时更详细的了解系统运行状态的东东；
+echoInfo() { echo "\033[1;36m$@\033[0m"; }      # info  重要，输出信息：用来反馈系统的当前状态给最终用户的；
+echoSuccess() { echo "\033[1;32m$@\033[0m"; }   # success 成功，输出信息：用来反馈系统的当前状态给最终用户的；
+echoWarn() { echo "\033[1;33m$@\033[0m"; }      # warn, 可修复，系统可继续运行下去；
+echoError() { echo "\033[1;31m$@\033[0m"; }     # error, 可修复性，但无法确定系统会正常的工作下去;
+echoFatal() { echo "\033[5;31m$@\033[0m"; }     # fatal, 相当严重，可以肯定这种错误已经无法修复，并且如果系统继续运行下去的话后果严重。
 
 # =========================================== HELP ===========================================
 help() {
-    echo_info "脚本名称: $SCRIPT_NAME"
-    echo_info "功能简介: $SCRIPT_DESC"
-    echo_info "当前版本: $CURRENT_VERSION"
-    echo_info "最近更新: $LAST_UPDATETIME"
-    echo_info "作    者: $AUTHER_NAME <$AUTHER_EMAIL>"
-    echo_info "说明文档: $REAMDME_URL"
-    echo_info
-    echo_info "============================================================="
-    echo_info
-    echo_info "Usage:"
-    echo_info
-    echo_info "\t\$ sh $SCRIPT_NAME [--Command] [-Option [value]] [--] [params...]"
-    echo_info
-    echo_info "Commands:"
-    echo_info "\tcommand1:\t\t命令1"
-    echo_info "\tcommand2:\t\t命令2"
-    echo_info
-    echo_info "Options:"
-    echo_info "\t--debug:\t打开调试模式."
-    echo_info "\t--verbose:\t显示更多的调试信息."
-    echo_info "\t--help:\t显示使用说明."
-    echo_info
-    echo_info "Params:"
-    echo_info "\tparam1:\t参数1"
-    echo_info "\tparam2:\t参数2"
+    echoInfo "脚本名称: $SCRIPT_NAME"
+    echoInfo "功能简介: $SCRIPT_DESC"
+    echoInfo "当前版本: $SCRIPT_VERSION"
+    echoInfo "最近更新: $SCRIPT_UPDATETIME"
+    echoInfo "作    者: $AUTHER_NAME <$AUTHER_EMAIL>"
+    echoInfo "说明文档: $REAMDME_URL"
+    echoInfo
+    echoInfo "============================================================="
+    echoInfo
+    echoInfo "Usage:"
+    echoInfo
+    echoInfo "\t\$ sh $SCRIPT_NAME [-dvh] [command] [params...] [--Option [value] [-sub_option [value]]...]..."
+    echoInfo
+    echoInfo "Commands:"
+    echoInfo "\tcommand1:\t命令1"
+    echoInfo "\tcommand2:\t命令2"
+    echoInfo
+    echoInfo "Options:"
+    echoInfo "\t--opt1:\t\t选项1"
+    echoInfo "\t--opt2:\t\t选项2"
+    echoInfo "\t--updatelog:\t脚本的更新日志"
+    echoInfo "\t--version:\t当前脚本版本"
+    echoInfo "\t--help:\t\t查看使用说明"
+    echoInfo
+    echoInfo "SubOptions:"
+    echoInfo "\t-sub_opt1:\t子选项1"
+    echoInfo "\t-sub_opt2:\t子选项2"
 }
 
 # =========================================== PROCESS ===========================================
 
 process() {
-    echo_success "Done !"
+    echoSuccess "收到参数 $#个：$@"
+    echoSuccess
+    echoSuccess "Done !"
     exit 0;
 }
 
 # =========================================== MAIN ===========================================
 
 main() {
-    echo_debug; echo_debug "======================== Date ======================"
-    echo_debug "$DATE"
-    echo_debug; echo_debug "======================== Command ======================"
-    args=("$@")
-    echo_debug "sh $0 $*"
-
-    if [[ -n $1 && ${1:0:1} == "-" ]]; then
-        echo_debug; echo_debug "======================== Parsing options ======================"
-        current_command_key=""
-        current_option_key=""
-        current_param_index=0
-        while [ -n "$1" ]; do
-            case "$1" in    
-                --) 
-                    current_command_key=""; current_option_key=""; current_param_index=0;
-                    shift
-                    break ;;    
-                --*)
-                    current_option_key=""; current_param_index=0;
-                    current_command_key="${1:2}"
-                    current_command_value="$([[ -z $2 || ${2:0:1} == "-" ]] && echo '1' || echo $2)";
-                    eval ${current_command_key}=$current_command_value
-                    echo_debug "$1\t=>\t\$$current_command_key = $current_command_value"
-                    shift $([[ -z $2 || ${2:0:1} == "-" ]] && echo 0 || echo 1);;
-                -*) 
-                    current_param_index=0;
-                    current_option_key_prefix=$([ $current_command_key ] && echo "${current_command_key}_" || echo "")
-                    current_option_key="${current_option_key_prefix}${1:1}"
-                    current_option_value=$([[ -z $2 || ${2:0:1} == "-" ]] && echo 1 || echo $2);
-                    eval ${current_option_key}=$current_option_value
-                    echo_debug "$1\t=>\t\$$current_option_key = $current_option_value"
-                    # shift ;;
-                    shift $([[ -z $2 || ${2:0:1} == "-" ]] && echo 0 || echo 1);;
-                *)
-                    current_param_key_prefix=${current_option_key:-$current_command_key}
-                    current_param_key_prefix=$([ $current_param_key_prefix ] && echo "${current_param_key_prefix}_" || echo "")
-                    current_param_key="${current_param_key_prefix}param${current_param_index}"
-                    current_param_value=$1
-                    eval ${current_param_key}=$current_param_value
-                    let current_param_index=$current_param_index+1
-                    echo_debug "$1\t=>\t\$$current_param_key = $current_param_value"
+    if [[ $1 && ${1:0:2} != "--" ]]; then
+        while getopts "dvh" OPT; do
+            case $OPT in
+                d) debug="1" ;;
+                v) verbose="1" ;;
+                h) help="1" ;;
+                ?) help; exit 1 ;;
             esac
-            shift
         done
+        shift $[$OPTIND-1]
     fi
 
-    echo_debug; echo_debug "======================== Parsing params ======================"
-    current_param_index=1
-    for param in "$@"; do
-        echo_debug "\$$current_param_index = $param"
-        current_param_index=$[ $current_param_index + 1 ]
+    echoDebug; 
+    echoDebug "======================== DATE ======================"
+    echoDebug "$DATE"
+    echoDebug; 
+    echoDebug "======================== GLOBAL CONST ======================"
+    echoDebug "\$MACOS_VER=$MACOS_VER"
+    echoDebug "\$TIMESTAMP=$TIMESTAMP"
+    echoDebug "\$DATE=$DATE"
+    echoDebug "\$DATE_STAMP=$DATE_STAMP"
+    echoDebug "\$CURRENT_PATH=$CURRENT_PATH"
+    echoDebug "\$SCRIPT_DIRPATH=$SCRIPT_DIRPATH"
+    echoDebug "\$SCRIPT_BASENAME=$SCRIPT_BASENAME"
+    echoDebug "\$SCRIPT_BASENAME_WITHOUT_SUFFIX=$SCRIPT_BASENAME_WITHOUT_SUFFIX"
+    echoDebug "\$SCRIPT_BASENAME_SUFFIX=$SCRIPT_BASENAME_SUFFIX"
+    echoDebug; 
+    echoDebug "======================== Command ======================"
+    echoDebug "sh $0 $*"
+    echoDebug; 
+    echoDebug "======================== Parsing options ======================"
+    commandParams=();
+    currentOptionKey=""
+    currentSubOptionKey=""
+    currentCommandParamIndex=-1
+
+    while [ -n "$1" ]; do
+        case "$1" in     
+            --*)
+                currentSubOptionKey="";
+                currentOptionKey="${1:2}"
+                currentOptionValue="$([[ -z $2 || ${2:0:1} == "-" ]] && echo '1' || echo $2)";
+                eval ${currentOptionKey}='$currentOptionValue'
+                echoDebug "\$$currentOptionKey=$currentOptionValue"
+                shift $([[ -z $2 || ${2:0:1} == "-" ]] && echo 0 || echo 1);;
+            -*) 
+                currentOptionKeyPrefix=$([ $currentOptionKey ] && echo "${currentOptionKey}_" || echo "")
+                currentSubOptionKey="${currentOptionKeyPrefix}${1:1}"
+                currentSubOptionValue=""
+                while [[ -n "$2" && ${2:0:1} != "-" ]]; do
+                    shift
+                    currentSubOptionValue=$([[ -z $currentSubOptionValue  ]] && echo "$1" || echo "$currentSubOptionValue $1")
+                done
+                echoDebug "\$$currentSubOptionKey=$currentSubOptionValue"
+                eval ${currentSubOptionKey}='$currentSubOptionValue'
+                shift $([[ -z $2 || ${2:0:1} == "-" ]] && echo 0 || echo 1);;
+            *)
+                let currentCommandParamIndex=$currentCommandParamIndex+1
+                commandParams[$currentCommandParamIndex]=$1
+                echoDebug "\$$currentCommandParamIndex=$1" 
+                # if $debug; then echoDebug "\$$currentCommandParamIndex=$1"; fi
+                # echoSuccess "commandParams count=${#commandParams[@]}，items=${commandParams[@]}"
+        esac
+        shift
     done
+    unset -v currentOptionKey; unset -v currentSubOptionKey; unset -v currentCommandParamIndex; 
 
-    echo_debug; echo_debug "======================== GLOBAL VARIABLES ======================"
-    echo_debug "debug = $debug"
-    echo_debug "verbose = $verbose"
-    echo_debug; echo_debug "=============================================="; echo_debug
+    echoDebug; echoDebug "======================== Params ======================"
+    currentParamIndex=1
+    echoDebug "\$0=$0"
+    for param in ${commandParams[@]}; do
+        echoDebug "\$$currentParamIndex=$param"
+        currentParamIndex=$[ $currentParamIndex + 1 ]
+    done
+    unset -v currentParamIndex
 
-    # 使用说明
-    if [ -z $help ]; then help; exit 0; fi;
+    echoDebug "======================== GLOBAL VARIABLES ======================"
+    echoDebug "debug=$debug"
+    echoDebug "verbose=$verbose"
+    echoDebug; 
+    echoDebug "=============================================="; echoDebug
+
+    # 预制处理
+    if [ $help ]; then help; exit 0; fi;
+    if [ $version ]; then echoInfo $SCRIPT_VERSION; exit 0; fi;
+    if [ $updatelog ]; then echoInfo $SCRIPT_UPDATE_LOG; exit 0; fi;
 
     # 开始处理
-    process $@ 
+    process ${commandParams[@]}
 }
 
 main $@
-
-
-# sh template.sh --command1 -option11 aaa -option12 bbb --command2 -option21 ccc -option22 ddd eee fff ggg
-# sh template.sh --command1 -option11 aaa xxx yyy -option12 bbb --command2 -option21 ccc -option22 ddd eee fff ggg
-# sh template.sh --command1 -option11 aaa "xxx yyy" -option12 bbb --command2 -option21 ccc -option22 ddd eee fff ggg
-# sh template.sh --command1 -option11 aaa xxx yyy -option12 bbb --command2 -option21 ccc -option22 ddd eee fff ggg
-# sh template.sh --command1 -option11 aaa -option12 bbb ddd eee fff ggg
-# sh template.sh --command1 -option11 -option12 bbb ddd eee fff ggg
-# sh template.sh --command1 -option11 -option12 -- bbb ddd eee fff ggg
-# sh template.sh --command1 ddd eee fff ggg
-# sh template.sh --command1
-# sh template.sh ddd eee fff ggg
-
-# sh template.sh aaa bbb --command1 
-
-# sh template.sh -exportArchive xxx -archivePath xcarchivepath -exportPath destinationpath -exportOptionsPlist plistpath
-# sh template.sh -exportArchive -archivePath xcarchivepath -exportPath destinationpath -exportOptionsPlist plistpath
-
-# sh template.sh --debug --verbose --build_target WKStudent-QA --build_branch dev1.1.6_build_script --build_app_version 1.1.6 --build_number 100 --build_config Release --env_wkserver https://appwk.baidu.com/ --env_proserver https://appwk.baidu.com/ --env_h5host https://tanbi.baidu.com/ --env_debugtool OFF --env_passport ONLINE --pod_update false --build_path false
-# sh template.sh --build_target WKStudent-QA --build_branch dev1.1.6_build_script --build_app_version 1.1.6 --build_number 100 --build_config Release --env_wkserver https://appwk.baidu.com/ --env_proserver https://appwk.baidu.com/ --env_h5host https://tanbi.baidu.com/ --env_debugtool OFF --env_passport ONLINE --pod_update false --build_path false
-
-# sh template.sh --build_target WKStudent-QA --build_branch dev1.1.6_build_script --build_app_version 1.1.6 --build_number 100 --build_config Release --env_wkserver https://appwk.baidu.com/ --env_proserver https://appwk.baidu.com/ --env_h5host https://tanbi.baidu.com/ --env_debugtool OFF --env_passport ONLINE --pod_update false --build_path false
-
-# [build_target] [build_branch] [build_app_version] [build_number] [build_config] [env_wkserver] [env_proserver] [env_h5host] [env_debugtool] [env_passport] [rmdir] [pod_update] [build_path]"
